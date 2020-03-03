c6e74ff46d2c2da9e2bc104141388898dc328446
CleonII <60782665+CleonII@users.noreply.github.com>
2020-03-03 18:19:19 +0100

    Update optimering_test.py

--- optimering_test.py
+++ optimering_test.py
@@ -21,7 +21,7 @@ import time
 # 3D matriser verkar inte vara långsammare än 2D, iallafall inte på sättet jag jämfört
 
 
-def model(t, y, kinetic_constants):
+def model_old(t, y, kinetic_constants):
     suc2 = y[0]
     hxk1 = y[1]
     mig1 = y[2]
@@ -36,7 +36,7 @@ def model(t, y, kinetic_constants):
     return dy_dt
 
 
-def data():
+def data_old():
     data_suc2 = np.array([1, 1.01867278, 1.03458747, 1.04816326, 1.05977747, 1.06977144, 1.0784309,  1.08590829,
                           1.09234407, 1.09788215, 1.10266992, 1.10685824, 1.11059849, 1.11395039, 1.11693021,
                           1.11957116, 1.12190974, 1.12398582, 1.12584262, 1.12752667, 1.12908718, 1.13054150,
@@ -90,83 +90,99 @@ def data():
     return data_conc
 
 
-def calc_sol_k(kinetic_constants_0):
+def model(t, y, kinetic_constants):
+    suc2 = y[0]
+    hxk1 = y[1]
+    r1 = suc2*kinetic_constants[0]
+    r2 = hxk1*kinetic_constants[1]
+    dsuc2_dt = -r1 + r2
+    dhxk1_dt = r1 - r2
+    dy_dt = [dsuc2_dt, dhxk1_dt]
+    return dy_dt
+
+
+def data():
+    kinetic_constants_true = [3, 6]
+    t_span = [0, 1]
+    y0 = [1, 1]
+    t_eval = np.linspace(0, 1, num=100)
+    sol = integrate.solve_ivp(fun=lambda t, y: model(t, y, kinetic_constants_true), t_span=t_span, y0=y0, method="RK45",
+                              t_eval=t_eval)
+    data_conc = np.empty([2, 100, 1])
+    data_conc[:, :, 0] = sol.y
+    return data_conc
+
+
+def calc_sol_k(kinetic_constants_0, constants):
+    num_coefficient, num_tidserier, num_tidsteg, h, t_span, t_eval, y0 = constants
     sol = integrate.solve_ivp(fun=lambda t, y: model(t, y, kinetic_constants_0), t_span=t_span, y0=y0, method="RK45",
                               t_eval=t_eval)
-    sol_k = np.empty([3, len(t_eval), 1])
+    sol_k = np.empty([num_tidserier, num_tidsteg, 1])
     sol_k[:, :, 0] = sol.y
-    # shape = (tidsserie(3), tidssteg(100), värde(1))
     return sol_k
 
 
-def calc_sol_k_step(kinetic_constants_0):
-    sol_k_step = np.empty([3, len(t_eval), num_coefficient])
+def calc_sol_k_step(kinetic_constants_0, constants):
+    num_coefficient, num_tidserier, num_tidsteg, h, t_span, t_eval, y0 = constants
+    sol_k_step = np.empty([num_tidserier, num_tidsteg, num_coefficient])
     for k in range(num_coefficient):
         kinetic_constants_step = kinetic_constants_0.copy()
         kinetic_constants_step[k] = kinetic_constants_step[k] + h
         sol = integrate.solve_ivp(fun=lambda t, y: model(t, y, kinetic_constants_step), t_span=t_span, y0=y0,
                                   method="RK45", t_eval=t_eval)
         sol_k_step[:, :, k] = sol.y
-    # shape = (tidsserie(3), tidssteg(100), värde i k-steg(4))
     return sol_k_step
 
 
-def calc_variance():
-    W = np.empty([3, 100, 100])
-    for tidserie in range(3):
-        W[tidserie, :, :] = np.identity(len(t_eval))
-    return W
+def calc_variance(constants):
+    num_coefficient, num_tidserier, num_tidsteg, h, t_span, t_eval, y0 = constants
+    mat_W = np.empty([num_tidserier, num_tidsteg, num_tidsteg])
+    for tidserie in range(num_tidserier):
+        mat_W[tidserie, :, :] = np.identity(num_tidsteg)
+    return mat_W
 
 
-def calc_gradient(sol_k, sol_k_step, W):
-    mat_r = calc_residual(sol_k)
-    diff_k_t = np.empty([3, len(t_eval), num_coefficient])
-    mat_A = np.empty([3, len(t_eval), num_coefficient])  # mat_A = S_T, S = sensitivity matrix
-    for k in range(num_coefficient):
-        diff_k_t[:, :, k] = sol_k_step[:, :, k] - solution_k[:, :, 0]
-        mat_A[:, :, k] = diff_k_t[:, :, k]/h
-    mat_A_T = np.transpose(mat_A, (0, 2, 1))  # mat_A_T = S, S = sensitivity matrix
-    # A.shape = (3, 100, 4)
-    # A_T.shape = (3, 4, 100)
-    # r.shape = (3, 100, 1)
-    # A_T.shape = (3, 4, 100)
-    # W.shape = (3, 100, 100)
-
+def calc_gradient(sol_k, sol_k_step, mat_W, constants, data_concentration):
     # grad = - 2 * A_T * W * r
-
-    mat_temp = np.matmul(mat_A_T, W, axes=[(-2, -1), (-2, -1), (-2, -1)])
+    num_coefficient, num_tidserier, num_tidsteg, h, t_span, t_eval, y0 = constants
+    mat_r = calc_residual(sol_k, constants, data_concentration)
+    diff_k_t = np.empty([num_tidserier, num_tidsteg, num_coefficient])
+    mat_A = np.empty([num_tidserier, num_tidsteg, num_coefficient])  # mat_A = S_T, S = sensitivity matrix
+    for k in range(num_coefficient):
+        diff_k_t[:, :, k] = sol_k_step[:, :, k] - sol_k[:, :, 0]
+        mat_A[:, :, k] = diff_k_t[:, :, k]/h
+    mat_A_transpose = np.transpose(mat_A, (0, 2, 1))  # mat_A_T = S, S = sensitivity matrix
+    mat_temp = np.matmul(mat_A_transpose, mat_W, axes=[(-2, -1), (-2, -1), (-2, -1)])
     # mat_temp.shape = (3, 4, 100)
     mat_temp2 = np.matmul(mat_temp, mat_r, axes=[(-2, -1), (-2, -1), (-2, -1)])
     # mat_temp2.shape = (3, 4, 1)
     grad = -2 * np.add.reduce(mat_temp2, 0)
     # grad.shape = (4, 1)
-    return grad, mat_A  # skicka med mat_A_T ?
+    return grad, mat_A, mat_A_transpose
 
 
-def calc_residual(sol_k):
-    mat_r = np.empty([3, len(t_eval), 1])
-    for tidsserie in range(3):
+def calc_residual(sol_k, constants, data_concentration):
+    num_coefficient, num_tidserier, num_tidsteg, h, t_span, t_eval, y0 = constants
+    mat_r = np.empty([num_tidserier, num_tidsteg, 1])
+    for tidsserie in range(num_tidserier):
         mat_r[tidsserie, :, 0] = data_concentration[tidsserie, :, 0] - sol_k[tidsserie, :, 0]
     return mat_r
 
 
-def calc_sum_residual(sol_k):
-    mat_r = calc_residual(sol_k)
+def calc_sum_residual(sol_k, constants, data_concentration):
+    mat_r = calc_residual(sol_k, constants, data_concentration)
     sum_res = np.sum(mat_r**2)
     return sum_res
 
 
-def calc_approximate_hessian(mat_A, mat_W):
-    mat_A_T = np.transpose(mat_A, (0, 2, 1))
-
+def calc_approximate_hessian(mat_A, mat_A_transpose, mat_W):
     # H = 2 * A_T * W * A
 
-    mat_temp = np.matmul(mat_A_T, mat_W, axes=[(-2, -1), (-2, -1), (-2, -1)])
-    # mat_temp.shape = (3, 4, 100)
+    mat_temp = np.matmul(mat_A_transpose, mat_W, axes=[(-2, -1), (-2, -1), (-2, -1)])
     mat_temp2 = np.matmul(mat_temp, mat_A, axes=[(-2, -1), (-2, -1), (-2, -1)])
-    # mat_temp2.shape = (3, 4, 4)
     hess_approx = 2 * np.add.reduce(mat_temp2, 0)
-    # hess_approx.shape = (4, 4)
+    cond_num = np.linalg.cond(hess_approx)
+    print(cond_num)
     return hess_approx
 
 
@@ -181,16 +197,17 @@ def calc_descent_direction(grad, inv_hess_approx):
     return p
 
 
-def calc_step(p, kinetic_constants_0, sol_k):
-    low_sum_res = calc_sum_residual(sol_k)
+def calc_step(p, kinetic_constants_0, sol_k, constants, data_concentration):
+    num_coefficient, num_tidserier, num_tidsteg, h, t_span, t_eval, y0 = constants
+    low_sum_res = calc_sum_residual(sol_k, constants, data_concentration)
     p_transpose = np.empty(num_coefficient)
     for k in range(num_coefficient):
         p_transpose[k] = p[k]
     best_step = eps
     for new_step_length in np.linspace(eps, 1, 100):
         kinetic_constants = kinetic_constants_0 + new_step_length*p_transpose
-        temp_sol_k = calc_sol_k(kinetic_constants)
-        temp_sum_res = calc_sum_residual(temp_sol_k)
+        temp_sol_k = calc_sol_k(kinetic_constants, constants)
+        temp_sum_res = calc_sum_residual(temp_sol_k, constants, data_concentration)
         if temp_sum_res < low_sum_res:
             best_step = new_step_length
         elif temp_sum_res > low_sum_res:
@@ -199,67 +216,79 @@ def calc_step(p, kinetic_constants_0, sol_k):
     return new_k, best_step
 
 
-def calc_step2(p, kinetic_constants_0, sol_k):
-    sum_res_0 = calc_sum_residual(sol_k)
+def calc_step2(p, kinetic_constants_0, sol_k, constants, data_concentration):
+    num_coefficient, num_tidserier, num_tidsteg, h, t_span, t_eval, y0 = constants
+    sum_res_0 = calc_sum_residual(sol_k, constants, data_concentration)
     p_transpose = np.empty(num_coefficient)
     for k in range(num_coefficient):
         p_transpose[k] = p[k]
-    best_step = 1
+    best_step = np.float32(1.0)
+    stop_iteration = False
     while True:
         kinetic_constants = kinetic_constants_0 + best_step * p_transpose
-        temp_sol_k = calc_sol_k(kinetic_constants)
-        temp_sum_res = calc_sum_residual(temp_sol_k)
+        temp_sol_k = calc_sol_k(kinetic_constants, constants)
+        temp_sum_res = calc_sum_residual(temp_sol_k, constants, data_concentration)
+        print(best_step)
         if temp_sum_res < sum_res_0:
             break
-        elif temp_sum_res > sum_res_0:
+        elif temp_sum_res >= sum_res_0:
             best_step = best_step/2
+        if best_step < 10**-10:
+            best_step = 0
+            stop_iteration = True
+            break
     new_k = kinetic_constants_0 + best_step * p_transpose
-    return new_k, best_step
+    return new_k, best_step, stop_iteration
 
-start = time.clock()
 
-for upprepning in range(1000):
-    K_array = np.array([15, 0, 3, 12], np.float64) # sant värde 4, 6, 7, 5
-    num_coefficient = len(K_array)
+def main():
+    k_array = np.array([11, 10], np.float64)
+    num_coefficient = len(k_array)
+    num_tidserier = 2
+    t_eval = np.linspace(0, 1, num=100)
+    num_tidsteg = len(t_eval)
     eps = np.finfo(float).eps
     h = np.sqrt(eps)
     t_span = [0, 1]
-    t_eval = np.linspace(0, 1, num=100)
-    SUC2_0 = 1
-    HXK1_0 = 1
-    MIG1_0 = 1
-    y0 = np.array([SUC2_0, HXK1_0, MIG1_0])
+    suc2_0 = 1
+    hxk1_0 = 1
+    y0 = np.array([suc2_0, hxk1_0])
+    constants = (num_coefficient, num_tidserier, num_tidsteg, h, t_span, t_eval, y0)
 
     data_concentration = data()
-
 
     gogogo = True
     iteration = 0
     while gogogo:
-        solution_k = calc_sol_k(K_array)
-        solution_k_step = calc_sol_k_step(K_array)
-        matrix_W = calc_variance()
-        gradient, matrix_A = calc_gradient(solution_k, solution_k_step, matrix_W)
-        approximated_hessian = calc_approximate_hessian(matrix_A, matrix_W)
+        solution_k = calc_sol_k(k_array, constants)
+        solution_k_step = calc_sol_k_step(k_array, constants)
+        matrix_w = calc_variance(constants)
+        gradient, matrix_a, matrix_a_transpose = calc_gradient(solution_k, solution_k_step, matrix_w, constants,
+                                                               data_concentration)
+        approximated_hessian = calc_approximate_hessian(matrix_a, matrix_a_transpose, matrix_w)
         inverted_hessian_approximation = calc_approx_inverted_hessian(approximated_hessian)
         descent_direction = calc_descent_direction(gradient, inverted_hessian_approximation)
-        new_K_array, step_length = calc_step2(descent_direction, K_array, solution_k)
-        K_array = new_K_array
-        sum_residue_0 = calc_sum_residual(solution_k)
-        if sum_residue_0 <= 10 ** -14:
+        new_k_array, step_length, stop_iteration = calc_step2(descent_direction, k_array, solution_k, constants, data_concentration)
+        k_array = new_k_array
+        sum_residue_0 = calc_sum_residual(solution_k, constants, data_concentration)
+        if sum_residue_0 <= 10 ** -15:
             gogogo = False
-            #print("Done!")
-            #print("Iterations = " + str(iteration))
-            #print("Residue = " + str(sum_residue_0))
-            #print("Coefficients = " + str(K_array))
+            print("Done!")
+            print("Iterations = " + str(iteration))
+            print("Residue = " + str(sum_residue_0))
+            print("Coefficients = " + str(k_array))
         iteration = iteration + 1
+        if stop_iteration:
+            print("Iteration stopped!")
+            print("Iterations = " + str(iteration))
+            print("Residue = " + str(sum_residue_0))
+            print("Coefficients = " + str(k_array))
+            break
         if iteration % 1000 == 0:
             print("Iterations = " + str(iteration))
             print("Residue = " + str(sum_residue_0))
-            print("Coefficients = " + str(K_array))
+            print("Coefficients = " + str(k_array))
 
-stop = time.clock()
 
-elapsed = stop - start
-print(elapsed)
+main()
 
