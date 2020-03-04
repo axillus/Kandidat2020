f9c22b12cece48fae466e7e6071f642730f810d3
CleonII <60782665+CleonII@users.noreply.github.com>
2020-03-04 08:49:20 +0100

    Update read_data.py

--- read_data.py
+++ read_data.py
@@ -1,4 +1,9 @@
 import pandas as pd
-def read_data(filename):
-    data_points = pd.read_csv(str(filename))
-   # print(df)
+
+
+def read_data():
+    data_points = pd.read_csv("Data_set_mean.csv")
+    print(data_points)
+
+
+read_data()
