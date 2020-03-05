20e203ee1dff1380a3c7493eb577a5a48b53ab4a
CleonII <60782665+CleonII@users.noreply.github.com>
2020-03-05 22:11:31 +0100

    Update read_data.py

--- read_data.py
+++ read_data.py
@@ -1,9 +1,15 @@
 import pandas as pd
+import numpy as np
 
 
-def read_data():
+def data():
     data_points = pd.read_csv("Data_set_mean.csv")
-    print(data_points)
+    data_points_np = np.transpose(data_points.values)
+    time_points = data_points_np[0, 0:29]
+    mig1_n_points = data_points_np[1, 0:29]
+    hxk1_points = data_points_np[2, 0:29]
+    suc2_points = data_points_np[3, 0:29]
+    data_conc = np.array([[mig1_n_points], [hxk1_points], [suc2_points]])
+    data_conc = np.transpose(data_conc, (0, 2, 1))
+    return time_points, data_conc
 
-
-read_data()
