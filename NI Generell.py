33964eb9ee71114ac2aafb8238421de959851b96
axillus <60795321+axillus@users.noreply.github.com>
2020-03-26 08:13:04 +0100

    Update NI Generell.py

--- NI Generell.py
+++ NI Generell.py
@@ -7,8 +7,8 @@ t_span = np.linspace(0, 240, num=100)
 y0 = [y01, y02]
 eps = np.finfo(float).eps
 d_Beta = math.sqrt(eps)
-Num_ekvationer=#Ekvationer
-Num_tidssteg=#tidssteg
+Num_eq=#Ekvationer
+Num_timestep=#tidssteg
 
 def simp_model(t,y,Kinetic_constants):
     SUC2=y[0]
@@ -25,7 +25,7 @@ def simp_model(t,y,Kinetic_constants):
 
 def sol():
     solv=integrate.solve_ivp(fun=lambda t, y: simp_model(t, y, Kinetic_constants), t_span=t_span, y0=y0, method="RK45")
-    solv_k = np.zeros([Num_ekvationer, Num_tidssteg, 1])
+    solv_k = np.zeros([Num_eq, Num_timestep, 1])
     solv_k[:, :, 0] = solv.y
     return solv_k
 
@@ -33,35 +33,34 @@ SUC2, Mig1, Mig1P=sol()
 
 
 def calc_S_mat():
-    s_SUC2= np.zeros((Num_tidssteg, len(Kinetic_constants)))
-    s_Mig1 = np.zeros((Num_tidssteg, len(Kinetic_constants)))
-    s_Mig1_P=np.zeros((Num_tidssteg, len(Kinetic_constants)))
-    dy = np.zeros([Num_ekvationer,Num_tidssteg,1])
+    s_SUC2= np.zeros((Num_timestep, len(Kinetic_constants)))
+    s_Mig1 = np.zeros((Num_timestep, len(Kinetic_constants)))
+    s_Mig1_P=np.zeros((Num_timestep, len(Kinetic_constants)))
+    dy = np.zeros([Num_eq,Num_timestep,1])
     for i in range(len(Kinetic_constants)):
         d_Kinetic_constants = Kinetic_constants.copy()
         d_Kinetic_constants[i] = d_Kinetic_constants[i] + d_Beta
         d_solv = integrate.solve_ivp(fun=lambda t, y: simp_model(t, y, d_Kinetic_constants), t_span=t_span, y0=y0,
                                    method="RK45")
-        d_solv_k = np.zeros([Num_ekvationer, Num_tidssteg, 1])
+        d_solv_k = np.zeros([Num_eq, Num_timestep, 1])
         d_solv_k[:, :, 0] = d_solv.y
         dy_new=dy.copy()
         dy_new[:]=d_solv_k
         print(dy_new)
         s_SUC2[:,i] = np.transpose((dy_new[0,:,:] - SUC2) / d_Beta)
         s_Mig1[:,i] = np.transpose((dy_new[1,:,:] - Mig1) / d_Beta)
-        s_Mig1_P[:, i] = np.transpose((dy_new[1, :, :] - Mig1_P) / d_Beta)
+        s_Mig1_P[:, i] = np.transpose((dy_new[2, :, :] - Mig1_P) / d_Beta)
         #print(s_A, s_B)
     return SUC2, Mig1, Mig1_P
 
-Sensitivity=np.array(calc_S_mat())
-print(Sensitivity.shape)
-def Variations_koefficient(Sensitivity):
-    S = Sensitivity.reshape(Num_tidssteg*Num_ekvationer,len(Kinetic_constants))
-    S_T=np.transpose(S)
+S=np.array(calc_S_mat())
+print(S.shape)
+def Variations_koefficient(S):
+    S_T=np.transpose(S,(0,2,1))
     H=2*np.matmul(S_T,S)
     H_inv=np.linalg.inv(H)
     print(H_inv)
-    C=np.diag(H_inv)
+    C=np.diag(H_inv[1])
     for i in range(len(Kinetic_constants)):
         Var_K=C/Kinetic_constants[i]
     return Var_K
