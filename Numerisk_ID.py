163939bc4985448dc3d194b0e743e28dd4ff6406
Viktor Hasselgren <60782665+CleonII@users.noreply.github.com>
2020-03-29 12:53:52 +0200

    Create Numerisk_ID.py

---
+++ Numerisk_ID.py
@@ -0,0 +1,51 @@
+#Tar fram Kovariansmatrisen, variationskoefficienter samt Korrelationsmatrisen
+
+import numpy as np
+from Main import Kinetic_constants
+from Main import Num_eq
+from Sensitivity import S
+
+def Var(S):
+    Var_K = np.zeros((Num_eq, len(Kinetic_constants),1))
+    S_T=np.transpose(S,(0,2,1))
+    H=2*np.matmul(S_T,S)
+    H_inv=np.linalg.inv(H)
+    for i in range(Num_eq):
+        for j in range(len(Kinetic_constants)):
+            for k in range(len(Kinetic_constants)):
+                C=np.array(np.diag(H_inv[i,:,:]))
+                Var_K[i,k,:]=C[k]/Kinetic_constants[j]
+    return H_inv, Var_K
+
+Covariance, Var_K = Var(S)
+
+
+def Corr(Covariance):
+    v0 = np.zeros((len(Kinetic_constants),1))
+    Correlation = np.zeros((2,len(Kinetic_constants),len(Kinetic_constants)))
+    for i in range(Num_eq):
+        v = v0.copy()
+        v = np.sqrt(np.diag(Covariance[i,:,:]))
+        outer_v = np.outer(v, v)
+        Correlation[i,:,:] = Covariance[i,:,:] / outer_v
+    return Correlation
+
+Correlation=Corr(Covariance)
+
+x_axis_labels=['K1', 'K2', 'K3']
+y_axis_labels=['K1', 'K2', 'K3']
+
+plt.subplot(221)
+Corr1=sns.heatmap(Correlation[0,:,:], vmin=-1, vmax=1, xticklabels=False, yticklabels=y_axis_labels).set_title('Korrelation')
+
+plt.subplot(222)
+Corr2=sns.heatmap(Correlation[1,:,:], vmin=-1, vmax=1, xticklabels=x_axis_labels, yticklabels=y_axis_labels)
+plt.show()
+
+plt.subplot(223)
+Corr2=sns.heatmap(Correlation[1,:,:], vmin=-1, vmax=1, xticklabels=x_axis_labels, yticklabels=y_axis_labels)
+plt.show()
+
+plt.subplot(224)
+Corr2=sns.heatmap(Correlation[1,:,:], vmin=-1, vmax=1, xticklabels=x_axis_labels, yticklabels=y_axis_labels)
+plt.show()
