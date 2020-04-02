42dfc9458cadde8955718ba2d719884c3c764fae
axillus <60795321+axillus@users.noreply.github.com>
2020-04-02 14:38:08 +0200

    Update Numerisk_ID.py

--- Numerisk_ID.py
+++ Numerisk_ID.py
@@ -5,6 +5,7 @@ from Main import Kinetic_constants
 from Main import Num_eq
 from Sensitivity import S
 import seaborn as sns
+import matplotlib.pyplot as plt
 
 def Var(S):
     Var_K = np.zeros((Num_eq, len(Kinetic_constants),1))
@@ -24,24 +25,12 @@ Covariance, Var_K = Var(S)
 
 def Corr(Covariance):
     v0 = np.zeros((len(Kinetic_constants),1))
-    Correlation = np.zeros((2,len(Kinetic_constants),len(Kinetic_constants)))
+    Correlation = np.zeros((Num_eq,len(Kinetic_constants),len(Kinetic_constants)))
     for i in range(Num_eq):
         v = v0.copy()
         v = np.sqrt(np.diag(Covariance[i,:,:]))
         outer_v = np.outer(v, v)
-        Correlation[Covariance == 0] = 0
         Correlation[i,:,:] = Covariance[i,:,:] / outer_v
     return Correlation
 
 Correlation=Corr(Covariance)
-
-x_axis_labels=['K1', 'K2', 'K3']
-y_axis_labels=['K1', 'K2', 'K3']
-
-plt.subplot(221)
-Corr_Mig1=sns.heatmap(Correlation[0,:,:], vmin=-1, vmax=1, xticklabels=False, yticklabels=y_axis_labels).set_title('Korrelation')
-
-
-plt.subplot(223)
-Corr_SUC2=sns.heatmap(Correlation[2,:,:], vmin=-1, vmax=1, xticklabels=x_axis_labels, yticklabels=y_axis_labels)
-plt.show()
