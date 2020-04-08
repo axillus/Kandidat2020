aca926246bd997cac1475c24f22ab5c8c485cc25
Viktor Hasselgren <60782665+CleonII@users.noreply.github.com>
2020-04-08 17:57:51 +0200

    Update read_data.py

--- read_data.py
+++ read_data.py
@@ -5,10 +5,26 @@ import numpy as np
 def data():
     data_points = pd.read_csv("Data_set_mean.csv")
     data_points_np = np.transpose(data_points.values)
-    time_points = data_points_np[0, 0:29]
-    mig1_n_points = data_points_np[1, 0:29]
-    hxk1_points = data_points_np[2, 0:29]
-    suc2_points = data_points_np[3, 0:29]
-    data_conc = np.array([[mig1_n_points], [hxk1_points], [suc2_points]])
-    data_conc = np.transpose(data_conc, (0, 2, 1))
+    time_points = data_points_np[0, :]
+    mig1_n_points = data_points_np[1, :]
+    hxk1_points = data_points_np[2, :]
+    suc2_points = data_points_np[3, :]
+    data_conc = np.transpose(np.array([[mig1_n_points], [hxk1_points], [suc2_points]]), (0, 2, 1))
+    return time_points, data_conc
+
+
+def full_data():
+    data_points = pd.read_csv("Data_set_mean.csv")
+    data_points_np = np.transpose(data_points.values)
+    time_points = data_points_np[0, :]
+    mig1_n_points = data_points_np[1, :]
+    hxk1_points = data_points_np[2, :]
+    suc2_points = data_points_np[3, :]
+    '''
+    time_points = np.nan_to_num(data_points_np[0, :])
+    mig1_n_points = np.nan_to_num(data_points_np[1, :])
+    hxk1_points = np.nan_to_num(data_points_np[2, :])
+    suc2_points = np.nan_to_num(data_points_np[3, :])
+    '''
+    data_conc = np.transpose(np.array([[mig1_n_points], [hxk1_points], [suc2_points]]), (0, 2, 1))
     return time_points, data_conc
