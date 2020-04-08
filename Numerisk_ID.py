9863e63a97f5f3f3cf9d065f59c9c50f7922de82
axillus <60795321+axillus@users.noreply.github.com>
2020-04-08 13:58:50 +0200

    Update Numerisk_ID.py

--- Numerisk_ID.py
+++ Numerisk_ID.py
@@ -1,24 +1,27 @@
 # Tar fram Kovariansmatrisen, variationskoefficienter samt Korrelationsmatrisen
 
 import numpy as np
-from Main import Kinetic_constants
-from Main import Num_eq
-from Sensitivity import S
-import seaborn as sns
-import matplotlib.pyplot as plt
 
-def Var(S):
-    Var_K = np.zeros((Num_eq, len(Kinetic_constants), 1))
+#from Main import Kinetic_constants
+#from Main import Num_eq
+from Sensitivity import S
+from model_version import model_info, num_coefficient
+from Optimering import iteration
+
+def Var(S, constants, results):
+    num_coefficient, num_tidserier, num_tidsteg, h = constants
+    Kinetic_constants = results
+    Var_K = np.zeros((num_tidserier, num_coefficient, 1))
     S_T = np.transpose(S, (0, 2, 1))
     H = 2*np.matmul(S_T, S)
     H_inv = np.linalg.inv(H)
-    for i in range(Num_eq):
-        c = np.sqrt(np.diag(H_inv[i, :, :]).reshape(len(Kinetic_constants), 1))
+    for i in range(num_tidserier):
+        c = np.sqrt(np.diag(H_inv[i, :, :]).reshape(num_coefficient, 1))
         for l in np.diag(H_inv[i]):
             if l < 0:
                 print('COV ej definierad')
                 break
-        for j in range(len(Kinetic_constants)):
+        for j in range(num_coefficient):
             Var_K[Kinetic_constants[j] == 0] = 0
             Var_K[i,j] = c[j] / Kinetic_constants[j]
     return H_inv, Var_K
@@ -26,10 +29,11 @@ def Var(S):
 
 Covariance, Var_K = Var(S)
 
-def Corr(Covariance):
-    v0 = np.zeros((len(Kinetic_constants), 1))
-    Correlation = np.zeros((Num_eq, len(Kinetic_constants), len(Kinetic_constants)))
-    for i in range(Num_eq):
+def Corr(Covariance, constants):
+    num_coefficient, num_tidserier, num_tidsteg, h = constants
+    v0 = np.zeros((num_coefficient, 1))
+    Correlation = np.zeros((Num_eq, num_coefficient, num_coefficient))
+    for i in range(num_tidserier):
         v = v0.copy()
         v = np.sqrt(np.diag(Covariance[i, :, :]))
         outer_v = np.outer(v, v)
