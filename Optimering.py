5bf8e92c1c7f0c83b58b85a7f552a8ec02ccf439
Viktor Hasselgren <60782665+CleonII@users.noreply.github.com>
2020-04-13 12:39:55 +0200

    Update Optimering.py

--- Optimering.py
+++ Optimering.py
@@ -207,7 +207,7 @@ def iteration(k_array, constants, data_concentration, data_info, ode_info):
 
 
 def save_results(results):
-    with open("fixad_model_1.csv", "a") as my_csv:
+    with open("viktad_model_1.csv", "a") as my_csv:
         csvWriter = csv.writer(my_csv, delimiter=",")
         csvWriter.writerow(results)
 
